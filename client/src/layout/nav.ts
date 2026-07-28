be33a91deb90d6fe0f37c40b9a3b38f9a7d31efb
aguerrero <guerrero.gf@gmail.com>
2026-07-28 16:22:21 -0600

    feat(ui): implement DaisyUI themes using Angular Signals

--- client/src/layout/nav.ts
+++ client/src/layout/nav.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../core/service/account-service';
 import { JsonPipe } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../core/service/toast-service';
+import { themes } from '../layout/theme';
 
 
 
@@ -13,11 +14,26 @@ import { ToastService } from '../core/service/toast-service';
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
+export class Nav implements OnInit {
   protected accountService= inject(AccountService)
   private router = inject(Router);
   private toast  = inject(ToastService)
   protected creds : any = {}
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light')
+  protected themes = themes; 
+
+  ngOnInit(): void{
+    document.documentElement.setAttribute('data-theme',this.selectedTheme());
+  }
+
+  handleSelectTheme(theme: string){
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme',theme);
+    const element = document.activeElement as HTMLDivElement
+    if (element) element.blur();
+
+  }
   
   login() {
     this.accountService.login(this.creds).subscribe({
